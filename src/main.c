57b897019b80c64e194460b9c4051c82f68287ff
Ramon Asuncion <ramon@b1t.dev>
2025-01-27 20:11:40 -0500

    Add exit menu (WIP).

--- src/main.c
+++ src/main.c
@@ -19,17 +19,17 @@ void init_ncurses()
 
 WINDOW *setup_window()
 {
-  int x_max, y_max;
-  int y_top, y_bottom, x_top, x_bottom;
+  int max_x, max_y;
+  int top_y, bottom_y, top_x, bottom_x;
 
-  getmaxyx(stdscr, y_max, x_max);
+  getmaxyx(stdscr, max_y, max_x);
 
-  y_bottom = y_max - 2;
-  x_bottom = x_max - 4;
-  y_top = 1;
-  x_top = 2;
+  bottom_y = max_y - 2;
+  bottom_x = max_x - 4;
+  top_y = 1;
+  top_x = 2;
 
-  WINDOW *win = newwin(y_bottom, x_bottom, y_top, x_top);
+  WINDOW *win = newwin(bottom_y, bottom_x, top_y, top_x);
   box(win, 0, 0);
   wrefresh(win);
 
@@ -88,14 +88,14 @@ void display_title(WINDOW *win)
   const char *title = "Recipe Manager";
   int title_length = strlen(title);
 
-  int x_max = getmaxx(win);
-  int title_x_pos = (x_max - title_length) / 2;
+  int max_x = getmaxx(win); // stdscr
+  int title_x_pos = (max_x - title_length) / 2;
 
   mvwprintw(win, 0, title_x_pos, "%s", title);
   wrefresh(win);
 }
 
-void display_menu(WINDOW *win)
+void display_menu(WINDOW *win, int selected_menu_item)
 {
   char *menu_options[] = {
     "Show",
@@ -105,20 +105,86 @@ void display_menu(WINDOW *win)
     "Copy",
   };
 
-  int y_max = getmaxy(win);
+  int max_y = getmaxy(win);
   int menu_count = sizeof(menu_options) / sizeof(menu_options[0]);
-  int menu_start_y = y_max - 1;
+  int menu_start_y = max_y - 1;
   int menu_start_x = 4;
 
   for (int i = 0; i < menu_count; ++i) {
-    wattron(win, A_BOLD | A_UNDERLINE);
-    mvwprintw(win, menu_start_y, menu_start_x, "%c", menu_options[i][0]);
-    wattroff(win, A_BOLD | A_UNDERLINE);
-    mvwprintw(win, menu_start_y, menu_start_x + 1, "%s", menu_options[i] + 1);
+    if (i == selected_menu_item) {
+      wattron(win, A_REVERSE | A_BOLD | A_UNDERLINE); // FIXME: why isn't it A_UNDERLINE when active? (https://www.google.com/url?sa=i&url=https%3A%2F%2Fgnosis.cx%2Fpublish%2Fprogramming%2Fcharming_python_6.html&psig=AOvVaw2sEX3ovu3axRFTijJ0H9cI&ust=1738076623421000&source=images&cd=vfe&opi=89978449&ved=0CBcQjhxqFwoTCLDnrY-WlosDFQAAAAAdAAAAABBY)
+      mvwprintw(win, menu_start_y, menu_start_x, "%c", menu_options[i][0]);
+      wattroff(win, A_REVERSE | A_BOLD | A_UNDERLINE);
+      mvwprintw(win, menu_start_y, menu_start_x + 1, "%s", menu_options[i] + 1);
+    } else {
+      wattron(win, A_BOLD | A_UNDERLINE);
+      mvwprintw(win, menu_start_y, menu_start_x, "%c", menu_options[i][0]);
+      wattroff(win, A_BOLD | A_UNDERLINE);
+      mvwprintw(win, menu_start_y, menu_start_x + 1, "%s", menu_options[i] + 1);
+    }
+
     menu_start_x += strlen(menu_options[i]) + 4;
   }
 
   wrefresh(win);
+}
+
+bool confirm_exit(WINDOW *win)
+{
+ int max_y, max_x;
+  getmaxyx(win, max_y, max_x);
+
+  int box_height = 5;
+  int box_width = 30;
+  int start_x = (max_x - box_width) / 2;
+  int start_y = (max_y - box_height) / 2;
+
+  WINDOW *exit_win = newwin(box_height, box_width, start_y, start_x);
+  box(exit_win, 0, 0);
+
+  const char *question = "Do you really want to exit?";
+  mvwprintw(exit_win, 1, 1, question);
+
+  wrefresh(exit_win);
+
+  int current_choice = 1; // yes
+  int ch = 0;
+
+  for (;;) {
+    const char *yes_option = "[Yes]";
+    const char *no_option = "[No]";
+
+    mvwprintw(exit_win, 3, (box_width - strlen(yes_option) - strlen(no_option) - 1) / 2, "%s", yes_option);
+    mvwprintw(exit_win, 3, (box_width + strlen(yes_option) + 1) / 2, "%s", no_option);
+
+    if (current_choice == 1) {
+      wattron(exit_win, A_REVERSE);
+      mvwprintw(exit_win, 3, (box_width - strlen(yes_option) - strlen(no_option) - 1) / 2, "%s", yes_option);
+      wattroff(exit_win, A_REVERSE);
+    } else {
+      wattron(exit_win, A_REVERSE);
+      mvwprintw(exit_win, 3, (box_width + strlen(yes_option) + 1) / 2, "%s", no_option);
+      wattroff(exit_win, A_REVERSE);
+    }
+
+    mvwprintw(exit_win, 4, 1, "Key pressed: %d", ch);
+
+    wrefresh(exit_win);
+
+    ch = wgetch(exit_win);
+    switch (ch) {
+      case KEY_LEFT:
+        current_choice = 1; // yes
+        break;
+      case KEY_RIGHT:
+        current_choice = 0; // no
+        break;
+      case '\n':
+      case KEY_ENTER:
+        delwin(exit_win);
+        return current_choice == 1;
+    }
+  }
 }
 
 void setup_sqlite_database(const char *db_name, sqlite3 **db)
@@ -159,6 +225,7 @@ int main(void)
 {
   WINDOW *window;
   sqlite3 *db;
+  int selected_menu_item = -1;
 
   setup_sqlite_database("recipes.db", &db);
 
@@ -171,27 +238,40 @@ int main(void)
   window = setup_window();
 
   display_title(window);
-  display_menu(window);
+  display_menu(window, selected_menu_item);
 
   const char *sql_query = "SELECT * FROM recipes";
   display_table(window, db, sql_query);
 
   char ch;
-  while ((ch = wgetch(window)) != 'q') {
+  while ((ch = tolower(wgetch(window)))) {
     switch (ch) {
-      case 'S': // show
+      case 's': // show
+        selected_menu_item = 0;
         break;
-      case 'A': // add
+      case 'a': // add
+        selected_menu_item = 1;
         break;
-      case 'E': // edit
+      case 'e': // edit
+        selected_menu_item = 2;
         break;
-      case 'D': // delete
+      case 'd': // delete
+        selected_menu_item = 3;
         break;
-      case 'C': // copy
+      case 'c': // copy
+        selected_menu_item = 4;
+        break;
+      case 'q': // quit
+        if (confirm_exit(window))
+          break;
+        else
+          continue;
         break;
       default:
+        selected_menu_item = -1;
         break;
     }
+    display_menu(window, selected_menu_item);
   }
 
   sqlite3_close(db);
